117ac435ab016ac5c126216be440d77b683f3bf6
yehey1999 <47969048+yehey1999@users.noreply.github.com>
2021-11-17 22:31:38 -0800

    Add postgres heroku as database

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,6 +5,7 @@ import { Event } from "./metro/entities/event.entity";
 import { Notification } from './metro/entities/notification.entity';
 import { Review } from './metro/entities/review.entity';
 import { Request } from './metro/entities/request.entity';
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 
 @Injectable()
 export class AppService {
@@ -16,17 +17,30 @@ export class AppService {
     return this.getDevConfig();
   }
 
-  private getDevConfig = (): SqliteConnectionOptions => {
+  private getDevConfig = (): PostgresConnectionOptions => {
     return {
-      type: 'sqlite',
-      database: 'metro-event-db',
+      // url: 'postgres://vetuelzgfedned:54753fb7f1aa43958bc8c365e22c8e1696a6c2a0422ba80c87844513eb5dab7d@ec2-52-201-195-11.compute-1.amazonaws.com:5432/daqliam7j4b1vj',
+      type: 'postgres',
+      host: 'ec2-52-201-195-11.compute-1.amazonaws.com',
+      port: 5432,
+      username: 'vetuelzgfedned',
+      password: '54753fb7f1aa43958bc8c365e22c8e1696a6c2a0422ba80c87844513eb5dab7d',
+      database: 'daqliam7j4b1vj',
       entities: [ User, Event, Notification, Review, Request ],
+      synchronize: false,
+      // multipleStatements: true,
       migrations: ["dist/migration/*.js"],
       cli: {
-        migrationsDir: "migration"
-      },    
-      synchronize: false,
-      
+        migrationsDir: "src/migration",
+      },
+      migrationsRun: true,
+      extra: { 
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      },
     };
   };
+
+  
 }
